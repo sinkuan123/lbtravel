ba4cc113d2415ee5d3657da1dd8fc7e6fbc28792
Lim Sin Kuan <limsinkuan123gmail.com>
2023-12-10 00:08:52 +0800

    ...

--- js/script.js
+++ js/script.js
@@ -4,3 +4,41 @@ function toggleFunction() {
         document.getElementById("return_detail").style.display = "block";
     }
 }
+function openMalaysia() {
+    document.getElementById("malaysia").style.display = "block";
+    document.getElementById("openMalaysia").style.display = "none";
+}
+
+function closeMalaysia() {
+    document.getElementById("malaysia").style.display = "none";
+    document.getElementById("openMalaysia").style.display = "block";
+}
+let menuOpen = false; // Initialize a variable to track the menu state
+
+function openNav() {
+    if (window.innerWidth < 900) {
+        document.getElementById("myNav").style.width = "100%";
+        menuOpen = true; // Set the menu state to open
+    } else {
+        document.getElementById("myNav").style.width = "30%";
+        document.getElementById("blur").classList.add("blur");
+        menuOpen = true; // Set the menu state to open
+    }
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+    document.getElementById("blur").classList.remove("blur");
+    menuOpen = false; // Set the menu state to closed
+}
+
+window.addEventListener("resize", function () {
+    if (menuOpen == true) {
+        // Only adjust the menu width if it wasn't explicitly opened
+        if (window.innerWidth < 900) {
+            document.getElementById("myNav").style.width = "100%";
+        } else {
+            document.getElementById("myNav").style.width = "30%";
+        }
+    }
+});
